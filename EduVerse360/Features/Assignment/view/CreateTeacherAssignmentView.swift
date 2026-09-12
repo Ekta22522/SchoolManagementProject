7de5caad75a9c49dc86f3c6c8181483b85f1e089
Ekta <ektarai826@gmail.com>
2026-09-12 21:05:34 +0545

    docs: navigation & routing redesign design spec

---
+++ EduVerse360/Features/Assignment/view/CreateTeacherAssignmentView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAssignmentView.swift
+//  EduVerse360
+//
+//  Created by Ekta Rai on 08/09/2026.
+//
+
+import SwiftUI
+
+struct CreateAssignmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAssignmentView()
+}
---
+++ EduVerse360/Features/Assignment/view/TeacherAssignmentByIdView.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherAssignmentByIdView.swift
+//  EduVerse360
+//
+//  Created by Ekta Rai on 10/09/2026.
+//
+
+import SwiftUI
+
+struct TeacherAssignmentByIdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeacherAssignmentByIdView()
+}
